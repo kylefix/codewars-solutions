77af558bd27a737738a65a9d228a232841d91f9f
Kyle <kyle@rapifix.com>
2018-06-25 10:03:45 -0400

    refactored

--- src/Calculator.js
+++ src/Calculator.js
@@ -32,105 +32,97 @@ const MULTIPLY = '*'
 const ADD = '+'
 const SUBSTRACT = '-'
 const DIVIDE = '/'
-const CLOSE_PARENS = ')'
-const OPEN_PARENS = '('
 const OPERATIONS = {
-  [MULTIPLY]:  (a, b) => a * b,
-  [ADD]:       (a, b) => a + b,
+  [MULTIPLY]: (a, b) => a * b,
+  [ADD]: (a, b) => a + b,
   [SUBSTRACT]: (a, b) => a - b,
-  [DIVIDE]:    (a, b) => a / b
+  [DIVIDE]: (a, b) => a / b
 }
 const ALL_OPERATORS = Object.keys(OPERATIONS)
+const evaluatePairWith = (a, b, operator) => OPERATIONS[operator](a, b)
+const evaluatePairAt = tokens =>
+  index => evaluatePairWith(tokens[index - 1], tokens[index + 1], tokens[index])
 
-const count = (arr, str) => arr.reduce((count, ch) => ch === str ? ++count : count, 0)
+const count = (arr, str) => arr.filter(ch => ch === str).length
 const indexOf = (str, ...searchValues) => str.findIndex(ch => searchValues.includes(ch))
 const push = (xs, ...x) => ((xs.push(...x), xs))
-const findLastIndex = (arr, fn) => {
-  const index = [...arr].reverse().findIndex(fn)
+const isAnOperator = str => ALL_OPERATORS.includes(str)
+const getSign = operators => Math.pow(-1, count(operators, SUBSTRACT))
+
+const replaceAt = ([...xs], start, deleteCount) =>
+  value => ((xs.splice(start, deleteCount, value), xs))
+
+const compose = (...fns) =>
+  initial => fns.reduceRight((args, fn) => fn(args), initial)
+
+const findLastIndex = ([...arr], fn) => {
+  const index = arr.reverse().findIndex(fn)
   return ~index ? arr.length + ~index : -1
 }
 
-const isAnOperator = str => ALL_OPERATORS.includes(str)
-
-// gets the indices of all OPEN_PARENS
-const allOpenParens = expression =>
-  [...expression]
-    .reduce((indices, ch, index) =>
-      ch === OPEN_PARENS ? push(indices, index) : indices
-      , [])
-
-const indexOfNextOperator = function * (tokens, operators, result) {
-  let nextIndex = indexOf(tokens, ...operators)
-  while (~nextIndex) {
-    yield nextIndex
-    // the caller will take care of removing the found operator from tokens
-    // probably not the best practice, but I think it's the clearest in this case
-    nextIndex = indexOf(tokens, ...operators)
-  }
-}
+const lastIndexNotIncluding = (xs, values) =>
+  findLastIndex(xs, item => !values.includes(item))
 
 // Only evaluates operators specified in the operators array
-const evaluateWithOperators = (tokens, operators) => {
-  for (const nextIndex of indexOfNextOperator(tokens, operators)) {
-    const evaluatePair = OPERATIONS[tokens[nextIndex]]
-    const result = evaluatePair(tokens[nextIndex - 1], tokens[nextIndex + 1])
-    // we mutate tokens directly here to get the next operator
-    tokens.splice(nextIndex - 1, 3, result)
-  }
+const evaluateTokensWith = (tokens, operators) => {
+  const nextIndex = indexOf(tokens, ...operators)
+  if (!~nextIndex) return tokens
 
-  return tokens
+  const evaluateWith = operators =>
+    tokens => evaluateTokensWith(tokens, operators)
+
+  const evaluateTokensWithAt = compose(
+    evaluateWith(operators),
+    replaceAt(tokens, nextIndex - 1, 3),
+    evaluatePairAt(tokens)
+  )
+
+  return evaluateTokensWithAt(nextIndex)
 }
 
-const getSign = operators => Math.pow(-1, count(operators, SUBSTRACT))
+const evaluateTokens = tokens =>
+  [[MULTIPLY, DIVIDE], [ADD, SUBSTRACT]]
+    .reduce(evaluateTokensWith, tokens)
+    .pop()
 
-const countLastConsecutiveOperators = (tokens) =>
-  tokens.length +
-  ~findLastIndex(tokens, token => !ALL_OPERATORS.includes(token))
+// reduce consecutive + and/or - to a single sign
+const reduceConsecutiveOperators = ([...tokens], token) => {
+  if (isAnOperator(token)) return push(tokens, token)
 
-const reduceConsecutiveOperators = (tokens, number) => {
-  const extraOperatorsCount = countLastConsecutiveOperators(tokens) - 1
-  if (!extraOperatorsCount) return push(tokens, number)
+  const extraOperatorsCount =
+    tokens.length + ~lastIndexNotIncluding(tokens, ALL_OPERATORS) - 1
+
+  if (!extraOperatorsCount) return push(tokens, +token)
+
   const operatorsToReduce = tokens.splice(-extraOperatorsCount)
 
-  return push(tokens, getSign(operatorsToReduce) * number)
+  return push(tokens, getSign(operatorsToReduce) * token)
 }
 
-const evaluateLeadingOperators = tokens =>
-  [SUBSTRACT, ADD].includes(tokens[0]) &&
-    tokens.unshift(0)
+// will turn [-,5] into [0,-,5]
+const fixLeadingOperator = tokens => {
+  [SUBSTRACT, ADD].includes(tokens[0]) && tokens.unshift(0)
+  return tokens
+}
 
 const parseTokens = expression =>
   expression
     .match(/([0-9.]+)|([-,+,*,/])/g)
-    .reduce((tokens, token) =>
-      isAnOperator(token)
-        ? push(tokens, token)
-        : (evaluateLeadingOperators(tokens, +token),
-          reduceConsecutiveOperators(tokens, +token))
-      , [])
+    .reduce(reduceConsecutiveOperators, [])
 
-// evaluates an expression without parenthesis
-const evaluate = expression =>
-  [[MULTIPLY, DIVIDE], [ADD, SUBSTRACT]]
-    .reduce(evaluateWithOperators, parseTokens(expression))
-    .pop()
+const evaluate = compose(
+  evaluateTokens,
+  fixLeadingOperator,
+  parseTokens
+)
 
-const evaluateSubExpressionAt = (expression, openParens) => {
-  const closeParens = expression.indexOf(CLOSE_PARENS, openParens)
-  const subExpression = expression.slice(openParens + 1, closeParens)
-  const result = evaluate(subExpression)
-
-  return expression.slice(0, openParens) +
-         result +
-         expression.slice(closeParens + 1)
-}
+const evaluateInnerMost = expression =>
+  expression.replace(
+    /(\((?!.*\().*?\))/, // look for inner most parens
+    match => evaluate(match.slice(1, -1))
+  )
 
 export const calc = expression =>
-  // first evaluate everything in between open and closed parenthesis
-  // they have to be evaluated in reverse order to make sure to always evaluate
-  // the inner most expression
-  evaluate(
-    allOpenParens(expression)
-      .reverse()
-      .reduce(evaluateSubExpressionAt, expression)
-  )
+  expression.includes('(')
+    ? calc(evaluateInnerMost(expression))
+    : evaluate(expression)
